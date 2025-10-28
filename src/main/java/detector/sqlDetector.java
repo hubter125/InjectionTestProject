22e4efbe2cebb7db94096ae6edde054e4c77cd86
hubter125 <hunter.korff@gmail.com>
2025-10-28 01:25:26 -0400

    Added doc Strings for methods

--- src/main/java/detector/sqlDetector.java
+++ src/main/java/detector/sqlDetector.java
@@ -11,6 +11,17 @@ public class sqlDetector implements Detector {
         this.name = name;
         this.signatures = new ArrayList<>();
     }
+
+    /**
+     * This method evaluates the liklihood of injection attacks being present based on static analysis
+     * 
+     * Reads a line of parsed file and matches expressions common in injection attacks
+     * 
+     * @param line The line of the file that we are currently reading
+     * 
+     * @return the total score, either 0 or 1 currently until further implementation is added
+     */
+
     @Override
     public int evaluateLine(String line){
         if (line == null) {
@@ -27,13 +38,38 @@ public class sqlDetector implements Detector {
         return score;
     }
 
+    /**
+     * Getter for total amount of hits
+     * 
+     * @return The number of signatures that were found in the log file for a given Detector
+     */
+
     public int getTotalHits(){
         return hits;
     }
-    
+
+    /**
+     * Getter for Name of Detector
+     * 
+     * This method gets the name of the Detector to be used in output formatting
+     * 
+     * @return String name of Detector
+     */
+
     public String getName(){
         return name;
     }
+
+    /**
+     * Reads a log file and stores it in an Array List
+     * 
+     * This method stores Strings in an ArrayList to be passed to evaluateLine()
+     * 
+     * @param String filepath is the filepath of the file you want to parse of injection attacks
+     * @return ArrayList<String> called signatures to store each line of the file for parsing in evaluateLine()
+     * @throws IOException if their is an error reading signatures file
+     */
+    
     public ArrayList<String> readList(String filepath) {
         signatures.clear();
         try (BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
